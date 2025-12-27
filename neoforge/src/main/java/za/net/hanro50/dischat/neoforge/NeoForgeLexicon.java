2e35d12f990ce2e4a214555d887f0592df8db472
Hanro50 <hlareman2@gmail.com>
2025-12-27 08:26:01 +0200

    Update NeoForgeLexicon.java

--- neoforge/src/main/java/za/net/hanro50/dischat/neoforge/NeoForgeLexicon.java
+++ neoforge/src/main/java/za/net/hanro50/dischat/neoforge/NeoForgeLexicon.java
@@ -2,23 +2,21 @@ package za.net.hanro50.dischat.neoforge;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
 
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.jarcontents.JarResource;
 import za.net.hanro50.dischat.core.Constants;
+import za.net.hanro50.dischat.core.Constants.MapContainer;
 import za.net.hanro50.dischat.core.Lexicon;
 import za.net.hanro50.dischat.core.NamespaceContainer;
 
 public class NeoForgeLexicon extends Lexicon {
-  protected void decode(JarResource resource, String origin) {
-    try {
-      String data = new String(resource.readAllBytes(), StandardCharsets.UTF_8);
 
-      var mc = Constants.GSON.fromJson("{\"map\":" + data + "}", LanguageInfo.class);
-      info.put(origin, mc);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  protected Map<String, String> decode(JarResource path, String origin) throws IOException {
+    return Constants.GSON.fromJson(Files.readString(path), MapContainer.class).getMap();
   }
 
   public NeoForgeLexicon(String version, String code) {
