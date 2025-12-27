f23c3dfb800177eec788f30b3deafdf400214001
Hanro50 <hlareman2@gmail.com>
2025-12-27 08:36:57 +0200

    Update NeoForgeLexicon.java

--- neoforge/src/main/java/za/net/hanro50/dischat/neoforge/NeoForgeLexicon.java
+++ neoforge/src/main/java/za/net/hanro50/dischat/neoforge/NeoForgeLexicon.java
@@ -2,8 +2,7 @@ package za.net.hanro50.dischat.neoforge;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
 
 import net.neoforged.fml.ModList;
@@ -14,9 +13,17 @@ import za.net.hanro50.dischat.core.Lexicon;
 import za.net.hanro50.dischat.core.NamespaceContainer;
 
 public class NeoForgeLexicon extends Lexicon {
+  protected Map<String, String> decode(JarResource resource, String origin) {
+    String data;
+    try {
+      data = new String(resource.readAllBytes(), StandardCharsets.UTF_8);
+      return Constants.GSON.fromJson(data, MapContainer.class).getMap();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
-  protected Map<String, String> decode(JarResource path, String origin) throws IOException {
-    return Constants.GSON.fromJson(Files.readString(path), MapContainer.class).getMap();
+    return new HashMap<>();
+
   }
 
   public NeoForgeLexicon(String version, String code) {
@@ -28,17 +35,19 @@ public class NeoForgeLexicon extends Lexicon {
       return super.retrieve(namespace);
     Constants.LOGGER.info("Loading language file from " + namespace.origin);
     var modFile = ModList.get().getModFileById(namespace.origin);
+    Map<String, String> result = new HashMap<>();
 
     var resource = modFile.getFile().getContents().get("/assets/" + namespace.origin + "/lang/" +
         this.lang + ".json");
     if (resource != null) {
-      decode(resource, namespace.origin);
+      result.putAll(decode(resource, namespace.origin));
       return super.retrieve(namespace);
     }
 
     resource = modFile.getFile().getContents().get("/assets/" + namespace.origin + "/lang/en_us.json");
     if (resource != null)
-      decode(resource, namespace.origin);
+      result.putAll(decode(resource, namespace.origin));
+    super.info.put(namespace.origin, result);
     return super.retrieve(namespace);
   }
 }
